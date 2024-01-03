fed86e42a1e8b9ea9ad2f177e699275d11840011
Tanathorn Piyachart <tangutang.tnp@gmail.com>
2024-01-03 16:40:36 +0700

    Project1

---
+++ ExchangeCurrency/script.js
@@ -0,0 +1,37 @@
+const currency_one = document.getElementById('currency-one');
+const currency_two = document.getElementById('currency-two');
+
+const amount_one = document.getElementById('amount-one');
+const amount_two = document.getElementById('amount-two');
+
+const rateText=document.getElementById('rate');
+const swap=document.getElementById('btn');
+
+
+currency_one.addEventListener('change',calculateMoney);
+currency_two.addEventListener('change',calculateMoney);
+amount_one.addEventListener('input',calculateMoney);
+amount_two.addEventListener('input',calculateMoney);
+
+
+function calculateMoney(){
+    const one=currency_one.value;
+    const two=currency_two.value;
+    //let url = `https://api.exchangerate-api.com/v4/latest/${one}`;
+    fetch(`https://api.exchangerate-api.com/v4/latest/${one}`)
+    .then(res=>res.json()).then(data=>{
+        //console.log(data.rates[two])
+        const rate=data.rates[two] ;
+        rateText.innerText=`1 ${one} = ${rate} ${two}`;
+        amount_two.value=(amount_one.value*rate).toFixed(2);
+    })
+}
+
+swap.addEventListener('click',()=>{
+    const temp = currency_one.value;
+    currency_one.value=currency_two.value;
+    currency_two.value = temp;
+    calculateMoney();
+})
+
+calculateMoney();
